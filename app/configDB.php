e94f22a3bc4ec47e1014e6e196ffcec7a44003ed
yurdmitriev <yurdmitriev02@gmail.com>
2022-03-16 14:24:54 +0200

    Fix database URL

--- app/configDB.php
+++ app/configDB.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 // -> BD Configs <-
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 define('DB_NAME', 'api_books');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
